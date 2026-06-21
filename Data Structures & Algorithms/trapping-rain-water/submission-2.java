d6e0e6f596dd16a05fd577acdf474060c0936898
E-A-1 <91286183+E-A-1@users.noreply.github.com>
2026-06-22 00:39:46 +0530

    Add: trapping-rain-water - submission-2

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-2.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int trap(int[] height) {
+       // Brute force solution
+       int res = 0;
+       if (height.length==0) {
+        return res;
+       }
+       int maxL = height[0];
+       int maxR = height[height.length-1];
+       int left = 1;
+       int right = height.length-2;
+       boolean lPass = maxL > maxR ? false : true;
+       while (left <=right) {
+        int response = 0;
+         if (maxL <= maxR ) {
+            maxL = Math.max(height[left], maxL);
+            response = maxL - height[left];
+            left++;
+         }
+         else {
+            maxR = Math.max(height[right], maxR);
+            response = maxR - height[right];
+            right--;
+         }
+
+         if (response > 0) {
+            res = res + response;
+         }
+       }
+
+       return res;
+    }
+}
