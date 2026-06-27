c71e5f0096bba0278ccf5c50dde34f1ba47ed5aa
E-A-1 <91286183+E-A-1@users.noreply.github.com>
2026-06-27 14:24:42 +0530

    Add: longest-repeating-substring-with-replacement - submission-0

---
+++ Data Structures & Algorithms/longest-repeating-substring-with-replacement/submission-0.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int characterReplacement(String s, int k) {
+        HashMap<Character, Integer> map = new HashMap<>();
+        int l=0;
+        int max = 0;
+        int res = 1;
+        for (int r=0; r<s.length(); r++) {
+            map.put(s.charAt(r), map.getOrDefault(s.charAt(r), 0)+1);
+            max = Math.max(max, map.get(s.charAt(r)));
+            while( ((r-l+1)- max) > k) {
+                map.put(s.charAt(l), map.getOrDefault(s.charAt(l), 0)-1);
+                l++;
+            }
+
+            res = Math.max(r-l+1, res);
+        }
+
+        return res;
+    }
+}
