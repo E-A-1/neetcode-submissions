0b94e9429b2bb4d84a472d917fc37aee3a4e7445
E-A-1 <91286183+E-A-1@users.noreply.github.com>
2026-06-28 17:23:15 +0530

    Add: permutation-string - submission-2

---
+++ Data Structures & Algorithms/permutation-string/submission-2.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        char c[] = s1.toCharArray();
+        Arrays.sort(c);
+        String sortedOne = Arrays.toString(c);
+        int l=0;
+        int r=s1.length()-1;
+        // Brute force one
+        while ( r < s2.length()) {
+            char c1[] = new char[s1.length()];
+            for (int i = 0; i<s1.length(); i++) {
+                c1[i] = s2.charAt(i+l);
+            }
+            Arrays.sort(c1);
+            String secondSorted = Arrays.toString(c1);
+
+            if (sortedOne.equals(secondSorted)) {
+                return true;
+            }
+            l++;
+            r++;
+        }
+
+
+        return false;
+    }
+}
