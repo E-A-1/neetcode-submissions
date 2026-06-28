9d12f448fe21e5f8a2f29695b2631c98b0d25b28
E-A-1 <91286183+E-A-1@users.noreply.github.com>
2026-06-28 17:47:52 +0530

    Add: permutation-string - submission-9

---
+++ Data Structures & Algorithms/permutation-string/submission-9.java
@@ -0,0 +1,47 @@
+class Solution {
+    public boolean checkInclusion(String s1, String s2) {
+        int[] freq1 = new int[26];
+        int[] freq2 = new int[26];
+        if (s1.length() > s2.length()) {
+            return false;
+        }
+        // Optimal Solution
+        for(int i = 0; i<s1.length();i++) {
+            freq1[s1.charAt(i)-'a']++;
+            freq2[s2.charAt(i)-'a']++;
+        }
+        int matches = 0;
+        for (int i=0;i<26;i++) {
+            if (freq1[i]==freq2[i]) {
+                matches++;
+            }
+        }
+        int l = 0;
+        for (int r = s1.length(); r<s2.length();r++){
+            if (matches == 26) {
+                return true;
+            }
+            int index = s2.charAt(r)-'a';
+            freq2[index]++;
+            if(freq2[index]==freq1[index]) {
+                matches++;
+            }
+            else if ((freq1[index]+1) == freq2[index]) {
+                matches--;
+            }
+            index = s2.charAt(l)-'a';
+            freq2[index]--;
+            if(freq2[index]==freq1[index]) {
+                matches++;
+            }
+            else if ((freq1[index]-1) == freq2[index]){
+                matches--;
+            }
+            l++;
+        }
+        if (matches ==26) {
+            return true;
+        }
+        return false;
+    }
+}
